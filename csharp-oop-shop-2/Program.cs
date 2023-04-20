176702080f70c195b85f61db106eb8a07e126342
JennyPersichini <nenny1096@gmail.com>
2023-04-20 18:02:08 +0200

    Add Elettrodomestico

--- csharp-oop-shop-2/Program.cs
+++ csharp-oop-shop-2/Program.cs
@@ -4,8 +4,9 @@
 using csharp_oop_shop_2;
 
 //prodotto(nome, descrizione, prezzo, iva, litri, ph, sorgente)
-
+/*
 Acqua bottiglia = new Acqua("Vitasnella", "Acqua naturale", 0.5, 5, 1.5, 7, "vitas");
+
 bottiglia.StampaProdotto();
 bottiglia.Bevi(0.9);
 bottiglia.StampaProdotto();
@@ -20,5 +21,11 @@ SacchettoFrutta Sacco = new("Mele", "Sacchetto di mele verdi", 3.5, 10, 4);
 
 Sacco.StampaProdotto();
 Sacco.MettiNelSacchetto(6);
-Sacco.StampaProdotto();
+Sacco.StampaProdotto();*/
 
+//prodotto(nome, descrizione, prezzo, iva, marca, colore, accesa = false)
+
+Elettrodomestico Lavatrice = new("Lavatrice", "Lava i panni", 260, 20, "Beko", "Bianca");
+
+Lavatrice.StampaProdotto();
+Lavatrice.Accenditi();
