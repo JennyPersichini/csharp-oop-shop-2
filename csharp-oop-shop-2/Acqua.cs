7002a2347e886b4860f3a5326c7a2a9f99b39ce6
JennyPersichini <nenny1096@gmail.com>
2023-04-20 15:11:19 +0200

    Add classe Acqua

--- csharp-oop-shop-2/Acqua.cs
+++ csharp-oop-shop-2/Acqua.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace csharp_oop_shop_2
 {
-    internal class Acqua
+    public class Acqua : Prodotto
     {
+        //ATTRIBUTI
+        private double litri = 1.5;
+        private int ph;
+        private string sorgente;
+
+        //COSTRUTTORE
+        public Acqua(string nome, string descrizione, double prezzo, double iva, double litri, int ph, string sorgente) : base(nome, descrizione, prezzo, iva)
+        {
+            this.litri = litri;
+            this.ph = ph;
+            this.sorgente = sorgente;
+        }
+        
+        //GETTERS
+        public double GetLitri()
+        {
+            return this.litri;
+        }
+
+        public double GetPh()
+        {
+            return this.ph;
+        }
+
+        public string GetSorgente()
+        {
+            return this.sorgente;
+        }
+
+        //METODI
+       
+
+
     }
 }
