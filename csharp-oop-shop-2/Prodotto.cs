3cee12c33752b82e8adc66a1dab8217b83bea8f0
JennyPersichini <nenny1096@gmail.com>
2023-04-20 14:07:41 +0200

    preso Prodotto.cs di ieri

--- csharp-oop-shop-2/Prodotto.cs
+++ csharp-oop-shop-2/Prodotto.cs
@@ -8,5 +8,51 @@ namespace csharp_oop_shop_2
 {
     internal class Prodotto
     {
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public double GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public double getPrezzoConIva()
+        {
+            return prezzo + (prezzo / 100 * iva);
+        }
+
+        public void CambiaPrezzo(double prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+
+        public string GetProdottoStringa()
+        {
+            string rapprStringa = "Nome: " + this.nome + ", ";
+            rapprStringa += "Descrizione: " + this.descrizione + ", ";
+            rapprStringa += "Prezzo: " + this.prezzo + ", ";
+
+            return rapprStringa;
+        }
     }
 }
